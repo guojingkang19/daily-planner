9a8ea57c42d9c1c21269c0d7a433382b2aa60a8c
guojingkang19 <jingkangG@outlook.com>
2026-07-26 01:57:14 +0800

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE = 'planner-v1';
+const ASSETS = [
+  '.',
+  'index.html',
+  'css/style.css',
+  'js/db.js',
+  'js/gestures.js',
+  'js/app.js',
+  'manifest.json',
+  'icons/icon-192.png',
+  'icons/icon-512.png',
+  'icons/icon-192-maskable.png',
+  'icons/icon-512-maskable.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
+
+self.addEventListener('notificationclick', e => {
+  e.notification.close();
+  const url = e.notification.data?.url || '.';
+  e.waitUntil(clients.openWindow(url));
+});
